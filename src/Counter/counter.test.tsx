4dafd90fc261d329289bfc46e98249f9b8e683bf
OkadaShotaro <b1016117@fun.ac.jp>
2019-03-27 15:28:06 +0900

    参考サイトの記述内容まで完了

--- src/Counter/counter.test.tsx
+++ src/Counter/counter.test.tsx
@@ -1,5 +1,5 @@
 import * as React from "react";
-import { render, cleanup } from "react-testing-library";
+import { render, cleanup, fireEvent } from "react-testing-library";
 
 import Counter from "./counter";
 
@@ -11,6 +11,14 @@ describe("Counter", () => {
     const { getByTestId } = render(<Counter />);
     expect(getByTestId("result").textContent).toBe("0");
   });
-  it('「+」ボタンを押すと１つカウントアップする', () => { })
-  it('「-」ボタンを押すと１つカウントダウンする', () => { })
+  it('「+」ボタンを押すと１つカウントアップする', () => { 
+    const { getByTestId, getByText } = render(<Counter />);
+    fireEvent.click(getByText("+"))
+    expect(getByTestId("result").textContent).toBe("1");
+  })
+  it('「-」ボタンを押すと１つカウントダウンする', () => { 
+    const { getByTestId, getByText } = render(<Counter />);
+    fireEvent.click(getByText("-"));
+    expect(getByTestId("result").textContent).toBe("-1");
+  })
 });
--- src/Counter/counter.tsx
+++ src/Counter/counter.tsx
@@ -1,2 +1,18 @@
-import * as React from 'react'
-export default () => <div data-testid="result">0</div>
+import * as React from "react";
+
+export default class Counter extends React.Component {
+  state = { count: 0 };
+  render() {
+    return (
+      <div>
+        <div data-testid="result">{this.state.count}</div>
+        <button onClick={() => this.setState({ count: this.state.count + 1 })}>
+          +
+        </button>
+        <button onClick={() => this.setState({ count: this.state.count - 1 })}>
+          -
+        </button>
+      </div>
+    );
+  }
+}
